1be15fa09bb5a0ccdf27ac25e0c39fa7731da278
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-27 16:20:38 -0500

    completed exercises 11-13

--- java_I/src/Main.java
+++ java_I/src/Main.java
@@ -34,8 +34,38 @@ public class Main {
         // int x = 5;
         // System.out.println(x++);
         // System.out.println(x);
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+        // int x = 5;
+        // System.out.println(++x);
+        // System.out.println(x);
+
+        // Exercise 11
+        // String class = "test"
+
+        // Exercise 12
+        // String theNumberThree = "three";
+        // Object o = theNumberThree;
+        // int three = (int) o;
+        // int three = (int) "three";
+
+        // Exercise 13
+        // int x = 4;
+        // x = x + 5;
+        // x += 5;
+        // System.out.println(x);
+
+        // int x = 3;
+        // int y = 4;
+        // y = y * x;
+        // y *= x;
+        // System.out.println(y);
+
+        // int x = 10;
+        // int y = 2;
+        // x = x / y;
+        // y = y - x;
+        // x /= y;
+        // y -= x;
+        // System.out.println(x);
+        // System.out.println(y);        
     }
 }
