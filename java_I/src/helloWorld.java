744994cf240ed9eb6def0eeb32e2433e68621f7a
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-27 16:51:50 -0500

    moved exercises to helloWorld file

--- java_I/src/helloWorld.java
+++ java_I/src/helloWorld.java
@@ -1,5 +1,74 @@
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // Exercise 1
+        int myFavoriteNumber = 38;
+        System.out.println(myFavoriteNumber);
+
+        // Exercise 2
+        String myString = "Codeup";
+        System.out.println(myString);
+
+        // Exercise 3
+        // String myString = 'C';
+
+        // Exercise 4
+        // String myString = 3.14159;
+
+        // Exercise 5
+        // long myNumber;
+        // System.out.println(myNumber);
+
+        // Exercise 6
+        // long myNumber = 3.14;
+
+        // Exercise 7
+        // long myNumberL = 3.14;
+
+        // Exercise 8 
+        // long myNumber = 123;
+
+        // Exercise 9
+        // float myNumber = 3.14;
+
+        // Exercise 10
+        // int x = 5;
+        // System.out.println(x++);
+        // System.out.println(x);
+        // int x = 5;
+        // System.out.println(++x);
+        // System.out.println(x);
+
+        // Exercise 11
+        // String class = "test"
+
+        // Exercise 12
+        // String theNumberThree = "three";
+        // Object o = theNumberThree;
+        // int three = (int) o;
+        // int three = (int) "three";
+
+        // Exercise 13
+        // int x = 4;
+        // x = x + 5;
+        // x += 5;
+        // System.out.println(x);
+
+        // int x = 3;
+        // int y = 4;
+        // y = y * x;
+        // y *= x;
+        // System.out.println(y);
+
+        // int x = 10;
+        // int y = 2;
+        // x = x / y;
+        // y = y - x;
+        // x /= y;
+        // y -= x;
+        // System.out.println(x);
+        // System.out.println(y);        
+            
     }
 }
