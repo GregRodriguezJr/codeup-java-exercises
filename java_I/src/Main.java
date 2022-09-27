5e0706334e7a9ed815fffee74d0d6c2840ab449c
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-26 21:28:40 -0500

    completed exercises 1-5

---
+++ java_I/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Exercise 1
+        int myFavoriteNumber = 38;
+        System.out.println(myFavoriteNumber);
+
+        // Exercise 2
+        String myString = "Codeup";
+        System.out.println(myString);
+
+        // Exercise 3
+        // String myString = 'C';
+
+        // Exercise 4
+        // String myString = 3.14159;
+
+        // Exercise 5
+        // long myNumber;
+        // System.out.println(myNumber);
+    }
+}
