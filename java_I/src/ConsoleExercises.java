dcbd339e3ace5eb8e6bae3542c26cd4c11065027
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-29 13:46:02 -0500

    completed exercise 1-scanner

--- java_I/src/ConsoleExercises.java
+++ java_I/src/ConsoleExercises.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
-        double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %.2f.\n", pi);
+        // Exercise 1
+        // double pi = 3.14159;
+        // System.out.printf("The value of pi is approximately %.2f.\n", pi);
+
+        // Scanner Class
+        // Exercise 1
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int userNum = scanner.nextInt();
+        System.out.println("The number you entered is: " + userNum);
+        scanner.close();
     }
 }
