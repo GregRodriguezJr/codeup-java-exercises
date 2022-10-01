b1019b5e5c19df7d3323c32fbe038807142db29f
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-01 14:18:19 -0500

    added new controlFlow class, completed exercise 1a

---
+++ java_I/src/ControlFlowExercises.java
@@ -0,0 +1,11 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // Loop Basics 1
+        // Exercise 1a - While
+        int i = 5;
+        while(i <= 15) {
+            System.out.println("Value of i is : " + i);
+            i++;
+        }
+    }
+}
