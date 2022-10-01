f290530884b086c2afcec34574d3ddf99f9e7b07
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-01 14:28:16 -0500

    completed 1b do while

--- java_I/src/ControlFlowExercises.java
+++ java_I/src/ControlFlowExercises.java
@@ -2,10 +2,17 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         // Loop Basics 1
         // Exercise 1a - While
-        int i = 5;
-        while(i <= 15) {
-            System.out.println("Value of i is : " + i);
-            i++;
-        }
+        // int i = 5;
+        // while(i <= 15) {
+        //     System.out.println("Value of i is : " + i);
+        //     i++;
+        // }
+
+        // Exercise 1b - Do While
+        int i = 0;
+        do {
+            System.out.println("Count by twos: " + i);
+            i += 2;
+        } while (i <= 100);
     }
 }
