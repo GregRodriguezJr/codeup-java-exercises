75dae7f225db0ce88f7cd9a06f4df9567c289bed
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-04 11:16:50 -0500

    completed exercise 4-dice roll

--- java_I/src/MethodsExercises.java
+++ java_I/src/MethodsExercises.java
@@ -21,34 +21,38 @@ public class MethodsExercises {
         // getInteger(userInput);
 
         // Exercise 3
-        boolean keepGoing = false;
-        boolean numValid;
+        // boolean keepGoing = false;
+        // boolean numValid;
     
-        do {
-            System.out.print("Enter a number between 1 - 10: ");
-            long userInput = scanner.nextLong();
+        // do {
+        //     System.out.print("Enter a number between 1 - 10: ");
+        //     long userInput = scanner.nextLong();
 
-            if (userInput >= 1 & userInput <= 10) {
-                numValid = true;
-                System.out.println("The factorial for " + userInput + " is " + getFactorial(userInput));
+        //     if (userInput >= 1 & userInput <= 10) {
+        //         numValid = true;
+        //         System.out.println("The factorial for " + userInput + " is " + getFactorial(userInput));
 
-                scanner.nextLine();
-                System.out.print("Would you like to continue? [y/n] ");
-                String answer = scanner.nextLine();
+        //         scanner.nextLine();
+        //         System.out.print("Would you like to continue? [y/n] ");
+        //         String answer = scanner.nextLine();
 
-                if (answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("y")) {
-                    keepGoing = true;
-                } else {
-                    keepGoing = false;
-                }
-            } else {
-                numValid = false;
-            }
+        //         if (answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("y")) {
+        //             keepGoing = true;
+        //         } else {
+        //             keepGoing = false;
+        //         }
+        //     } else {
+        //         numValid = false;
+        //     }
 
-        } while (!numValid || keepGoing);
+        // } while (!numValid || keepGoing);
+
+        // Exercise 4
+        diceRoll();
 
         scanner.close();
     }
+
     // Exercise 1 - Basic Arithmetic
     // public static double addition(double num1, double num2) {
     //     return num1 + num2;
@@ -81,11 +85,37 @@ public class MethodsExercises {
     // }
 
     // Exercise 3 method
-    public static long getFactorial(long num) {
-        long factorial = 1;
-        for(int i = 1; i <= num; ++i) {
-            factorial *= i;
-        }
-        return factorial;
-    }  
+    // public static long getFactorial(long num) {
+    //     long factorial = 1;
+    //     for(int i = 1; i <= num; ++i) {
+    //         factorial *= i;
+    //     }
+    //     return factorial;
+    // }  
+
+    // Exercise 4
+    public static void diceRoll() {
+        Scanner scanner = new Scanner(System.in);
+        boolean keepGoing;
+        do {
+            System.out.print("How many sides on your dice? ");
+            int sides = scanner.nextInt();
+            int randomSide1 = (int)Math.floor(Math.random() * sides + 1);
+            int randomSide2 = (int)Math.floor(Math.random() * sides + 1);
+            System.out.println("First die roll: " + randomSide1);
+            System.out.println("Second die roll: " + randomSide2);
+
+            System.out.print("Would you like to play again? [y/n] ");
+            scanner.nextLine();
+            String answer = scanner.nextLine();
+            
+            if(answer.equalsIgnoreCase("y")) {
+                keepGoing = true;
+            } else {
+                keepGoing = false;
+            }
+        } while (keepGoing);
+        
+        scanner.close();
+    }
 }
