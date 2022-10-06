ba3e990fad548e36c86cbe0f7c44a8abeb5bc7b4
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-06 15:38:06 -0500

    added getters for Input class

--- java_II/src/util/Input.java
+++ java_II/src/util/Input.java
@@ -3,5 +3,29 @@ import java.util.Scanner;
 
 // Exercise 1 - Create an input validation class
 public class Input {
-    
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String getString() {
+        System.out.print("Enter a string: ");
+        return scanner.nextLine();
+    }
+
+    public static boolean yesNo() {
+        System.out.print("Enter a [y/n]: ");
+        String userInput = scanner.nextLine();
+        return userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y");
+    }
+
+    public static int getInt(int min, int max) {
+        System.out.printf("Enter a between %s and %s ", min, max);
+        int userNum = scanner.nextInt(); 
+        if(userNum > min && userNum < max) {
+            getInt(min, max);
+        }
+        return userNum;
+    }
+
+    public static int getInt() {
+        return scanner.nextInt();
+    }
 }
--- java_II/src/util/InputTest.java
+++ java_II/src/util/InputTest.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class InputTest {
     public static void main(String[] args) {
-
+        // System.out.println("You entered: " + Input.getString());
+        // System.out.println(Input.yesNo());
+        System.out.println(Input.getInt());;
     }
 }
