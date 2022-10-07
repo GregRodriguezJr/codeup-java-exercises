f960cd1c5a2d8b4411df3d6513a855946f5f0e94
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-07 13:51:56 -0500

    created Rectangle class with fields/constructor/methods

---
+++ java_II/src/Shapes/Rectangle.java
@@ -0,0 +1,19 @@
+package Shapes;
+
+public class Rectangle {
+    protected long lenght;
+    protected long width;
+
+    public Rectangle(long lenght, long width) {
+        this.lenght = lenght;
+        this.width = width;
+    }
+
+    public long getPerimeter() {
+        return (2 * this.lenght) + (2 * this.width);
+    }
+
+    public long getArea() {
+        return this.lenght * this.width;
+    }
+}
