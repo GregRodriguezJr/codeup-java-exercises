6c2c646febeaacbed91ed02eac9e31aa83084f19
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-10 08:55:27 -0500

    completed exercise 1 - Shapes

---
+++ java_II/src/Shapes/Square.java
@@ -0,0 +1,16 @@
+package Shapes;
+
+public class Square extends Rectangle {
+
+    public Square(long side) {
+        super(side,side);
+    }
+
+    public long getArea() {
+        return (long) Math.pow(lenght,2);
+    }
+    
+    public long getPerimeter() {
+        return 4 * width;
+    }
+}
