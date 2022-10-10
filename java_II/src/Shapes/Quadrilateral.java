de6e48687f2b09d6a445db7c4a6cb7451885a992
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-10 12:40:37 -0500

    completed quadrilateral class

--- java_II/src/Shapes/Quadrilateral.java
+++ java_II/src/Shapes/Quadrilateral.java
@@ -1,5 +1,23 @@
 package Shapes;
 
 public abstract class Quadrilateral extends Shape implements Measurable{
-    
+    protected long lenght;
+    protected long width;
+
+    public Quadrilateral(long lenght, long width) {
+        this.lenght = lenght;
+        this.width = width;
+    }
+
+    public long getLenght() {
+        return lenght;
+    }
+
+    public long getWidth() {
+        return width;
+    }
+
+    public abstract void setLenght(long lenght);
+
+    public abstract void setWidth(long width);
 }
