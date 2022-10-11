8307c6a43ab3f0a06c4c12906d83e835190285fd
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-11 13:42:28 -0500

    created new generator class, added two arrays

---
+++ java_II/src/ServerNameGenerator.java
@@ -0,0 +1,9 @@
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+        String [] adjectives = {"victorious", "proud", "witty", "plain", "clean", "elegant", "fit", "jolly", "lively", "silly" };
+
+        String [] nouns = {"fire", "detail", "change", "act", "design", "iron", "flight", "kick", "land", "jelly"};
+
+        
+    }
+}
