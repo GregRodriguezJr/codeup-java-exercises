53927b4f8a550aa077b930b34b43352060e1689a
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-11 13:59:53 -0500

    completed and tested exercise 2-server name generator

--- java_II/src/ServerNameGenerator.java
+++ java_II/src/ServerNameGenerator.java
@@ -1,9 +1,19 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
     public static void main(String[] args) {
         String [] adjectives = {"victorious", "proud", "witty", "plain", "clean", "elegant", "fit", "jolly", "lively", "silly" };
 
         String [] nouns = {"fire", "detail", "change", "act", "design", "iron", "flight", "kick", "land", "jelly"};
 
-        
+        String random1 = ServerNameGenerator.getName(adjectives);
+        String random2 = ServerNameGenerator.getName(nouns);
+
+        System.out.println("Here is your server name:\n" + random2 + "-" + random1);
+    }
+
+    public static String getName(String [] arr) {
+        int randomInt = new Random().nextInt(arr.length);
+        return arr[randomInt];
     }
 }
