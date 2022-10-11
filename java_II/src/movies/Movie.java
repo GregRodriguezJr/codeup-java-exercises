7d7dfaa7bfda5b9b7442894b6357bea02efcb972
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-11 15:13:32 -0500

    created movies class with methods/getters/setters

---
+++ java_II/src/movies/Movie.java
@@ -0,0 +1,28 @@
+package movies;
+
+public class Movie {
+    private String name;
+    private String catagory;
+
+    public Movie(String name, String catagory) {
+        this.name = name;
+        this.catagory = catagory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCatagory() {
+        return catagory;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCatagory(String catagory) {
+        this.catagory = catagory;
+    }
+    
+}
