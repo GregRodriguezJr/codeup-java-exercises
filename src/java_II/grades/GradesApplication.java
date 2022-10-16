66b9a7669d96564cb7dac9f4cd82a1a8b693ead5
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-16 13:47:06 -0500

    created grades application class with new hashmap

---
+++ src/java_II/grades/GradesApplication.java
@@ -0,0 +1,10 @@
+package java_II.grades;
+
+import java.util.HashMap;
+
+public class GradesApplication {
+    public static void main(String[] args) {
+        HashMap<String, Student> students = new HashMap<>();
+
+    }
+}
--- src/java_II/grades/StudentTest.java
+++ src/java_II/grades/StudentTest.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student student1 = new Student("Mclovin");
-        student1.addGrade(50);
-        student1.addGrade(75);
-        student1.addGrade(60);
-        Student.printStudent(student1);
+//        Student student1 = new Student("Mclovin");
+//        student1.addGrade(50);
+//        student1.addGrade(75);
+//        student1.addGrade(60);
+//        Student.printStudent(student1);
     }
 }
